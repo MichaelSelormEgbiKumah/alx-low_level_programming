9def08e920381269345e1c2d08efc4df8617fa3a
MichaelSelormEgbiKumah <egbikumahmichael@gmailcom>
2023-10-21 02:52:42 -0700

    5-printf.c corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 #include <stdio.h>
 /*
-=======
- #include <stdio.h>
-/**
->>>>>>> d711a2c814da5c953eedb16f67449a92c5e5d252
- * * main - this is a prinf function
- * * Return: 0
- * */
+  * main - this is a prinf function
+  * Return: 0
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
