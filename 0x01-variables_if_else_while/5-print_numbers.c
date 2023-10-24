f9a12e1c2e034c3ebc048ade0886825c7f3701f9
MichaelSelormEgbiKumah <egbikumahmichael@gmailcom>
2023-10-24 00:40:51 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
-  * main - program that prints the alphabet in lowercase and upper case
-  * Return: Always 0.
-  **/
+*main - Prints all single digits of base 10 followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
 	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
+		putchar(digit);
 	}
 	putchar('\n');
 	return (0);
